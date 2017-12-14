c526b97e1df81b6fe7ce855f4528395518c9e0f0
mssngr <gabriel@ecliptic.io>
2017-12-14 13:21:11 -0700

    Added check for today's date.

--- qandaSend12PM.js
+++ qandaSend12PM.js
@@ -1,47 +1,50 @@
-'use latest';
-import twilio from 'twilio'
+'use latest'; // eslint-disable-line
+import Twilio from 'twilio'
 import {request} from 'graphql-request'
 
-export default (context, cb) => {
-  const {
-    TWILIO_ACCT_SID,
-    TWILIO_AUTH_TOKEN,
-    TWILIO_PHONE,
-    GRAPHCOOL_SIMPLE_API_END_POINT,
-    GRAPHCOOL_WEBTASK_AUTH_TOKEN,
-  } = context.secrets
-  const twilioClient = new twilio(TWILIO_ACCT_SID, TWILIO_AUTH_TOKEN)
+const todayObject = new Date()
+const today = `${todayObject.getMonth() + 1}/${todayObject.getDate()}`
 
-  const getMessageData = `{
+export default (context, cb) => {
+	const {
+		TWILIO_ACCT_SID,
+		TWILIO_AUTH_TOKEN,
+		TWILIO_PHONE,
+		GRAPHCOOL_SIMPLE_API_END_POINT,
+	} = context.secrets
+	const twilioClient = new Twilio(TWILIO_ACCT_SID, TWILIO_AUTH_TOKEN)
+
+	const getMessageData = `{
     allUsers {
       phone
       firstName
     }
-    Question(dateToAsk: "12/14") {
+    Question(dateToAsk: ${today}) {
       text
     }
   }`
 
-  const errors = []
-  const messages = []
+	const errors = []
+	const messages = []
 
-  request(GRAPHCOOL_SIMPLE_API_END_POINT, getMessageData)
-    .then(data => {
-      data.allUsers.forEach(user => {
-        twilioClient.messages.create({
-        	to: user.phone,
-        	from: TWILIO_PHONE,
-        	body: `Hello, ${user.firstName}. ${data.Question.text}`,
-        }, (err, message) => {
-        	if (err) {
-        		errors.push(err)
-        	} else {
-        	  messages.push(message.sid)
-        	}
-        })
-      })
-    })
+	request(GRAPHCOOL_SIMPLE_API_END_POINT, getMessageData)
+		.then(data => {
+			data.allUsers.forEach(user => {
+				twilioClient.messages.create({
+					to: user.phone,
+					from: TWILIO_PHONE,
+					body: `Hello, ${user.firstName}. ${data.Question.text}`,
+				}, (err, message) => {
+					if (err) {
+						errors.push(err)
+					}
+					else {
+						messages.push(message.sid)
+					}
+				})
+			})
+		})
 
-  const anyErrors = (errors.length > 0) ? errors : null
-  cb(anyErrors, messages)
+	const anyErrors = (errors.length > 0) ? errors : null
+	cb(anyErrors, messages)
 }
