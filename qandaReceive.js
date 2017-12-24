ef0a9cbe9e1052a38208dab8d94ca1f3e52438f2
mssngr <gabriel@ecliptic.io>
2017-12-24 13:47:25 -0700

    Connected qandaReceive to account setup.

--- qandaReceive.js
+++ qandaReceive.js
@@ -114,46 +114,19 @@ export default (context, cb) => {
 			// If there is a User connected to the phone number...
 			if (User) {
 
-				/* PENDING PARTNER REQUEST */
-				// Check if they have a pending partner request...
-				if (User.pendingPartnerPhone) {
-					// ...Check to see if they accepted it or declined it.
-					if (body.contains('accept')) {
-						// If they accepted, get the Partner's data...
-						request(GRAPHCOOL_SIMPLE_API_END_POINT, getUserByPhone(User.pendingPartnerPhone))
-							.then(partnerData => {
-								const Partner = partnerData.User
-								// ...And set up the connection between the two Users.
-								request(GRAPHCOOL_SIMPLE_API_END_POINT, setPartner(User.id, Partner.id))
-									// Then send a message to both Users, signalling success.
-									.then(() => {
-										sendSMS(`Congrats! You and ${Partner.firstName} are connected. When either of you replies to a Daily Question, the other will be sent the answer. As the years go by, you'll also be reminded of previous years' answers. Have fun!`, User.phone)
-										sendSMS(`Congrats! ${User.firstName} accepted your partner request. When either of you replies to a Daily Question, the other will be sent the answer. As the years go by, you'll also be reminded of previous years' answers. Have fun!`, Partner.phone)
-									})
-									.catch(error => errors.push(error))
-							})
-					} else if (body.contains('decline')) {
-						// If they declined, send a message to the requester and delete the request.
-					} else {
-						// If their message doesn't accept or decline,
-						// ask them about the pending partner, again.
-					}
-				}
-
 				/* ACCOUNT SETUP */
 				// Check if they've completed the account set up.
 				if (User.accountSetupStage < 5) {
-					// If they haven't, let's shoot them and their message over to account setup
-					startWebtask('qandaAccountSetup', {user: User, })
+					// If they haven't, let's shoot them and their message data over to account setup
+					startWebtask('qandaAccountSetup', {User, userMessageData: data})
 				}
 
 				const currentDate = moment().tz(User.timezone)
 				const today = `${currentDate.month() + 1}/${currentDate.date()}`
 				request(GRAPHCOOL_SIMPLE_API_END_POINT, getQuestionByDate(today))
-
-			// If there's not a User connected to the phone number...
 			} else {
-				// ...Create one
+				// If there's not a User connected to the phone number,
+				// ask if the sender wants to create an account
 				request(GRAPHCOOL_SIMPLE_API_END_POINT, createUser(data.From, data.FromZip))
 					.catch(error => errors.push(error))
 				// And send a welcome message out to the new user.
@@ -168,6 +141,32 @@ export default (context, cb) => {
 						messages.push(message)
 					}
 				})
+			}
+
+			/* PENDING PARTNER REQUEST */
+			// Check if they have a pending partner request...
+			if (User.pendingPartnerPhone) {
+				// ...Check to see if they accepted it or declined it.
+				if (body.contains('accept')) {
+					// If they accepted, get the Partner's data...
+					request(GRAPHCOOL_SIMPLE_API_END_POINT, getUserByPhone(User.pendingPartnerPhone))
+						.then(partnerData => {
+							const Partner = partnerData.User
+							// ...And set up the connection between the two Users.
+							request(GRAPHCOOL_SIMPLE_API_END_POINT, setPartner(User.id, Partner.id))
+								// Then send a message to both Users, signalling success.
+								.then(() => {
+									sendSMS(`Congrats! You and ${Partner.firstName} are connected. When either of you replies to a Daily Question, the other will be sent the answer. As the years go by, you'll also be reminded of previous years' answers. Have fun!`, User.phone)
+									sendSMS(`Congrats! ${User.firstName} accepted your partner request. When either of you replies to a Daily Question, the other will be sent the answer. As the years go by, you'll also be reminded of previous years' answers. Have fun!`, Partner.phone)
+								})
+								.catch(error => errors.push(error))
+						})
+				} else if (body.contains('decline')) {
+					// If they declined, send a message to the requester and delete the request.
+				} else {
+					// If their message doesn't accept or decline,
+					// ask them about the pending partner, again.
+				}
 			}
 
 			// Check for errors and send any with the callback.
